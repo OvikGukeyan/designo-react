ad5798e197954456104298903234e21cf890e57c
OvikGukeyan <105244612+OvikGukeyan@users.noreply.github.com>
2023-12-14 21:10:27 +0100

    adaptiv-mobile-

--- src/Pages/Projects/index.tsx
+++ src/Pages/Projects/index.tsx
@@ -21,61 +21,66 @@ const Projects: React.FC<ProjectsType> = ({ handleChooseCategory, category }) =>
     const [isLoading, setIsLoading] = useState(false);
 
 
-    useEffect(() => { (
-        async function () {
-            setIsLoading(true)
-            try {
-                const {data} = await axios.get(`https://64fa17ff4098a7f2fc156145.mockapi.io/designo?${category !== null ? `category=${category}` : ''}`)
-                setProjects(data)
-                setIsLoading(false)
-            }catch(error) {
-                console.log(error)
-                setIsLoading(false)
+    useEffect(() => {
+        (
+            async function () {
+                setIsLoading(true)
+                try {
+                    const { data } = await axios.get(`https://64fa17ff4098a7f2fc156145.mockapi.io/designo?${category !== null ? `category=${category}` : ''}`)
+                    setProjects(data)
+                    setIsLoading(false)
+                } catch (error) {
+                    console.log(error)
+                    setIsLoading(false)
+                }
             }
-        }
-    )();
+        )();
         // setIsLoading(true)
         // axios.get(`https://64fa17ff4098a7f2fc156145.mockapi.io/designo?${category !== null ? `category=${category}` : ''}`)
         //     .then(res => setProjects(res.data))
-        
+
     }, [category])
 
     console.log(projects, category)
 
     return (
-            <div className={styles.projects}>
-                {category === 'web_design' &&
-                    <div className={`${styles.title_board} ${styles[category ? category : '']}`}>
-                        <h1>Web Design</h1>
-                        <p>We build websites that serve as powerful marketing tools <br />
-                            and bring memorable brand experiences.</p>
-                    </div>}
-                {category === 'app_design' &&
-                    <div className={`${styles.title_board} ${styles[category ? category : '']}`}>
-                        <h1>App Design</h1>
-                        <p>Our mobile designs bring intuitive digital solutions
-                            to your customers right at their fingertips.</p>
-                    </div>}
-                {category === 'graphic_design' &&
-                    <div className={`${styles.title_board} ${styles[category ? category : '']}`}>
-                        <h1>Graphic Design</h1>
-                        <p>We deliver eye-catching branding materials that are
-                            tailored to meet your business objectives.
-                        </p>
-                    </div>}
-                <div className={styles.projects_items}>
-                    {!isLoading ? projects?.map((project) => (
-                        <ProjectItem project={project} />
+        <div className={styles.projects}>
+            {category === 'web_design' &&
+                <div className={`${styles.title_board} ${styles[category ? category : '']}`}>
+                    <h1>Web Design</h1>
+                    <p>We build websites that serve as powerful marketing tools <br />
+                        and bring memorable brand experiences.</p>
+                </div>}
+            {category === 'app_design' &&
+                <div className={`${styles.title_board} ${styles[category ? category : '']}`}>
+                    <h1>App Design</h1>
+                    <p>Our mobile designs bring intuitive digital solutions
+                        to your customers right at their fingertips.</p>
+                </div>}
+            {category === 'graphic_design' &&
+                <div className={`${styles.title_board} ${styles[category ? category : '']}`}>
+                    <h1>Graphic Design</h1>
+                    <p>We deliver eye-catching branding materials that are
+                        tailored to meet your business objectives.
+                    </p>
+                </div>}
+            <div className={styles.projects_items}>
+                {!isLoading ? projects?.map((project) => (
+                    <ProjectItem project={project} />
+                ))
+                    :
+                    [...Array(6)].map((i) => (
+                        <ItemLoader />
                     ))
-                        :
-                        [...Array(6)].map((i) => (
-                            <ItemLoader />
-                        ))
-                    }
+                }
 
-                </div>
-                <NavBoard handleChooseCategory={handleChooseCategory} className={category ? category : 'home'} />
             </div>
+            <div className={styles.nav_wrapper}>
+                <NavBoard handleChooseCategory={handleChooseCategory} className={category ? category : 'home'} />
+
+            </div>
+
+        </div>
 
     )
 }
